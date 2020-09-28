a5e22fd4683569cb99a232ea8c5fa4c822d33d21
rem029 <elawrenceponce@gmail.com>
2020-09-28 08:42:08 +0300

    Updates

---
+++ precache-manifest.2ac63ac3aae613ba178196e013d8bf49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96e6980ef7711c6b72f06222d9373c57",
+    "url": "/AutoType/index.html"
+  },
+  {
+    "revision": "b32599add67d772ff536",
+    "url": "/AutoType/static/css/main.4352cb43.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/AutoType/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "b32599add67d772ff536",
+    "url": "/AutoType/static/js/main.62989274.chunk.js"
+  },
+  {
+    "revision": "2b5a6516e7d2f9b5baf0",
+    "url": "/AutoType/static/js/runtime~main.d1eccbcb.js"
+  }
+]);
